677cdb824d96f4f5d5ee77d8330dc31651db7bbd
Kjell Harald <kjell.kodehode@gmail.com>
2022-09-12 13:56:48 +0200

    push all

---
+++ js/kalk.js
@@ -0,0 +1,5 @@
+const pxem = document.getElementById("pxem");
+addEventListener("click", function pxem() {
+  sum = px / 16;
+  const pxput = (document.getElementById("pxput").innerHTML = sum);
+});
