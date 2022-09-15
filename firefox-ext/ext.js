b989193f7efdca0ad104a0febe01f2880ad4d63f
Kjell Harald <kjell.kodehode@gmail.com>
2022-09-15 13:47:01 +0200

    push all

--- firefox-ext/ext.js
+++ firefox-ext/ext.js
@@ -6,43 +6,12 @@ const rmfirst = document.getElementById("rmfirst");
 const rmlast = document.getElementById("rmlast");
 const wrap = document.getElementById("footer");
 
-if (store.lenght === undefined && localStorage.getItem("store") !== null) {
-  store = JSON.parse(localStorage.getItem("store"));
-  render();
-}
-
-save.addEventListener("click", function save() {
-  pushstorage();
-});
-
-rmfirst.addEventListener("click", function rmfirst() {
-  store.shift();
-  render();
-});
-
-rmlast.addEventListener("click", function rmlast() {
-  store.pop();
-  render();
-});
-
-add.addEventListener("click", function add() {
-  let tabQuery = chrome.tabs.query(
-    { currentWindow: true, active: true },
-    function (tabs) {
-      let url = tabs[0].url;
-      store.push(url);
-      pushstorage();
-      render();
-    }
-  );
-});
-
-function download() {
+const download = () => {
   wrap.innerHTML = "";
-  let base64 = btoa(JSON.stringify(store));
-  let dl = `<a id="dl" href="data:application/octet-stream;charset=utf-16le;base64,${base64}">Download text file</a>`;
+  const base64 = btoa(JSON.stringify(store));
+  const dl = `<a id="dl" href="data:application/octet-stream;charset=utf-16le;base64,${base64}">Download text file</a>`;
   wrap.innerHTML = dl;
-}
+};
 
 function pushstorage() {
   localStorage.setItem("store", JSON.stringify(store));
@@ -52,10 +21,37 @@ function render() {
   btn.innerHTML = "";
   let links = "";
   for (let i = 0; i <= store.length; i++) {
-    if (store[i] !== undefined) {
-      links += `<p><a href="${store[i]}" target="_blank">${store[i]}</a></p>`;
+    if (store[i]) {
+      links += `<a href="${store[i]}" target="_blank">${store[i]}</a>`;
     }
   }
   download();
   btn.innerHTML = links;
 }
+
+if (!store.lenght && localStorage.getItem("store")) {
+  console.log(localStorage.getItem("store"));
+  store = JSON.parse(localStorage.getItem("store"));
+  render();
+}
+
+save.addEventListener("click", pushstorage);
+
+rmfirst.addEventListener("click", () => {
+  store.shift();
+  render();
+});
+
+rmlast.addEventListener("click", () => {
+  store.pop();
+  render();
+});
+
+add.addEventListener("click", () => {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    const url = tabs[0].url;
+    store.push(url);
+    pushstorage();
+    render();
+  });
+});
